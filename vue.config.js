20802867fdea1b3d4670be999d7160b091617db1
Thorsten Krug <email@donkeymedia.eu>
2020-03-09 20:22:06 +0100

    Added abstract global SCSS files for Vars and Mixins.

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,14 @@ module.exports = {
   devServer: {
     host: "0.0.0.0",
     port: "4396"
+  },
+  css: {
+    loaderOptions: {
+      sass: {
+        // Globally import some scss vars in all components.
+        prependData: `@import "@/assets/style/_variables.scss"; @import "@/assets/style/_mixins.scss";`
+      }
+    }
   }
+
 };
